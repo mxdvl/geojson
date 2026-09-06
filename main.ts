966e5ee2aba1a84627a6b8b30487070f134059ed
Max Duval <hi@mxdvl.com>
2026-09-06 22:40:01 +0100

    clearer position

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-/** Either 2 or 3 dimensions, as per RFC */
+/** Coordinates with either 2 or 3 dimensions, as per [§3.1.1](https://www.rfc-editor.org/info/rfc7946/#section-3.1.1) */
 export type Position =
-  | readonly [number, number]
-  | readonly [number, number, number];
+  | readonly [longitude: number, latitude: number]
+  | readonly [longitude: number, latitude: number, altitude: number];
